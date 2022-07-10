eeefcf3a39c9053903eae31370d7b51ceb2ee220
Nahuel Fernandez Beschtedt <lnahuelfernandezb@gmail.com>
2022-07-10 18:57:20 -0300

    Comienzo de la seccion sobre mi

--- src/pages/sobreMi.tsx
+++ src/pages/sobreMi.tsx
@@ -3,10 +3,12 @@ import styles from './styles/sobreMi.module.css'
 const SobreMi = () => {
   return (
     <div className={styles.container} id='sobreMi'>
-      <a id="sobreMi"></a>
-      <h1 className={styles.title}>
-        sobreMi
-      </h1>
+      <h2 className={styles.title}>Sobre mi</h2>
+      <p>Estudié y me recibí en la Facultad de Arquitectura, Diseño y Urbanismo (FADU) de la UBA en la carrera de Diseño Gráfico. 
+    
+    Me gusta trabajar de modo personalizado en cada proyecto y para cada cliente, atendiendo a las necesidades particulares de cada caso, por eso mis trabajos son bastante heterogéneos.
+    
+    Fundamento mi trabajo en la creatividad y en la responsabilidad de cumplir con los requerimientos solicitados en tiempo y forma e involucrándome en cada proyecto para aportar lo mejor, tanto en la imagen como en la comunicación y los conocimientos relacionados que he adquirido con mi experiencia.</p>
     </div>
   )
 }
