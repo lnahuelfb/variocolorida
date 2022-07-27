77386928b24ebaeb0a49c39bbfc337ad45284acc
Nahuel Fernandez Beschtedt <lnahuelfernandezb@gmail.com>
2022-07-26 22:06:22 -0300

    Te odio CORS x9

--- pages/api/send-email.js
+++ pages/api/send-email.js
@@ -23,7 +23,6 @@ export default async function handler(req, res) {
     'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE'
   };
 
-
   await runMiddleware(req, res, cors)
 
   if (req.method === 'OPTIONS') { return res.status(200).json((headers, { body: "OK" })) }
