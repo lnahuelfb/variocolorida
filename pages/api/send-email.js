ae3fa58e3c3430b00ff77bda65cae9472ac3febc
Nahuel Fernandez Beschtedt <lnahuelfernandezb@gmail.com>
2022-07-31 19:36:56 -0300

    Aparente arreglo de CORS x8

--- pages/api/send-email.js
+++ pages/api/send-email.js
@@ -48,7 +48,7 @@ export default async function handler(req, res) {
 
         return res.status(201).send('Email envíado!')
       } catch (error) {
-        return res.status(500).json({message})
+        return res.status(500).json({message: error.message})
       }
 
     case ('GET'):
