e6e0c6a1491609d8574d9fd74e9f64e9f033a48c
Nahuel Fernandez Beschtedt <lnahuelfernandezb@gmail.com>
2022-07-31 21:20:27 -0300

    Supuesto arreglo de Cors x22

--- pages/api/send-email.js
+++ pages/api/send-email.js
@@ -6,8 +6,9 @@ import mail from './_utils'
 
 export default async function handler(req, res) {
 
-  res.setHeader("Access-Control-Allow-Origin", '*')
+  res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Content-Type', 'application/json')
+  res.setHeader('Access-Control-Allow-Methods', 'POST')
 
   await NextCors(req, res, {
     methods: ['POST', 'GET']
