d69e10863269bf4aaf52d94a0885549c3d352520
Nahuel Fernandez Beschtedt <lnahuelfernandezb@gmail.com>
2022-08-08 03:56:31 -0300

    Cambio en la API x15

--- pages/admin/[seccion]/index.jsx
+++ pages/admin/[seccion]/index.jsx
@@ -4,64 +4,64 @@ import AdminCard from 'components/adminCard'
 
 import styles from 'styles/secciones.module.css'
 
-const Seccion = ({ data }) => {
+// const Seccion = ({ data }) => {
 
-  const router = useRouter()
-  const { seccion } = router.query
+//   const router = useRouter()
+//   const { seccion } = router.query
 
-  return (
-    <div className={styles.container}>
-      <h1>{data.seccion}</h1>
-      <div className={styles.cardContainer}>
-        {
-          data.trabajos && data.trabajos.map(({ name, image, id }) => (
-            <AdminCard name={name} image={image} link={`${seccion}/${id}`} key={id} />
-          ))
-        }
-      </div>
-    </div>
-  )
-}
+//   return (
+//     <div className={styles.container}>
+//       <h1>{data.seccion}</h1>
+//       <div className={styles.cardContainer}>
+//         {
+//           data.trabajos && data.trabajos.map(({ name, image, id }) => (
+//             <AdminCard name={name} image={image} link={`${seccion}/${id}`} key={id} />
+//           ))
+//         }
+//       </div>
+//     </div>
+//   )
+// }
 
-export async function getStaticProps({ params }) {
-  require('dotenv').config()
+// export async function getStaticProps({ params }) {
+//   require('dotenv').config()
 
-  const API = process.env.API || 'http://localhost:3000/api/'
+//   const API = process.env.API || 'http://localhost:3000/api/'
 
-  try {
-    const res = await fetch(`${API}${params.seccion}`)
-    const data = await res.json()
+//   try {
+//     const res = await fetch(`${API}${params.seccion}`)
+//     const data = await res.json()
 
-    return {
-      props: {
-        data
-      }
-    }
-  } catch (error) {
-    console.log(error)
-  }
-}
+//     return {
+//       props: {
+//         data
+//       }
+//     }
+//   } catch (error) {
+//     console.log(error)
+//   }
+// }
 
-export async function getStaticPaths() {
-  require('dotenv').config()
+// export async function getStaticPaths() {
+//   require('dotenv').config()
 
-  const API = process.env.API || 'http://localhost:3000/api/'
+//   const API = process.env.API || 'http://localhost:3000/api/'
 
-  try {
-    const res = await fetch(`${API}data`)
-    const data = await res.json()
+//   try {
+//     const res = await fetch(`${API}data`)
+//     const data = await res.json()
 
-    const paths = data.map(({ link }) => ({ params: { seccion: link } }))
+//     const paths = data.map(({ link }) => ({ params: { seccion: link } }))
 
-    return {
-      paths,
-      fallback: false,
-    }
-  } catch (error) {
-    console.log(error)
-    return { paths: [], fallback: false }
-  }
+//     return {
+//       paths,
+//       fallback: false,
+//     }
+//   } catch (error) {
+//     console.log(error)
+//     return { paths: [], fallback: false }
+//   }
 
-}
+// }
 
 export default Seccion
