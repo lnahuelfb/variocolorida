153c43e386344b97cf3e8ddea189a18cac469ec3
Nahuel Fernandez Beschtedt <lnahuelfernandezb@gmail.com>
2022-08-19 19:22:55 -0300

    Arreglos en los links de admin/home x5

--- pages/admin/[seccion]/[name].jsx
+++ pages/admin/[seccion]/[name].jsx
@@ -2,92 +2,89 @@ import Image from 'next/image'
 
 import styles from '/styles/adminName.module.css'
 
-// const Trabajo = ({ data }) => {
-const Trabajo = () => {
+const Trabajo = ({ data }) => {
 
   return (
     <div className={styles.container}>
-      {/* <h1>Hola ID: {data.name}</h1> */}
-      <h1>Hola ID:</h1>
-      {/* <Image src={data.image} width={400} height={400} objectFit='scale-down' alt={data.name} /> */}
-
+      <h1>Hola ID: {data.name}</h1>
+      <Image src={data.image} width={400} height={400} objectFit='scale-down' alt={data.name} />
     </div>
   )
 }
 
-// export async function getStaticProps({ params: { seccion, name } }) {
-//   require('dotenv').config()
+export async function getStaticProps({ params: { seccion, name } }) {
+  require('dotenv').config()
 
-//   const API = process.env.API || 'http://localhost:3000/api/'
+  const API = process.env.API || 'http://localhost:3000/api/'
 
-//   try {
-//     const res = await fetch(`${API}${seccion}/${name}`)
-//     const data = await res.json()
+  try {
+    const res = await fetch(`${API}${seccion}/${name}`)
+    const data = await res.json()
 
-//     return {
-//       props: {
-//         data
-//       }
-//     }
-//   } catch (error) {
-//     console.log(error)
-//   }
-// }
+    return {
+      props: {
+        data
+      }
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
 
-// export async function getStaticPaths() {
-//   require('dotenv').config()
+export async function getStaticPaths() {
+  require('dotenv').config()
 
-//   const API = process.env.API || 'http://localhost:3000/api/'
+  const API = process.env.API || 'http://localhost:3000/api/'
 
-//   try {
-//     const paths = []
+  try {
+    const paths = []
 
-//     const ilustracion = await fetch(`${API}ilustracion`)
-//     const ilustracionData = await ilustracion.json()
+    const ilustracion = await fetch(`${API}ilustracion`)
+    const ilustracionData = await ilustracion.json()
 
 
-//     ilustracionData.trabajos.map(({ name }) => {
-//       return paths.push({
-//         params: {
-//           seccion: 'ilustracion',
-//           name: name.toLowerCase()
-//         }
-//       })
-//     })
+    ilustracionData.trabajos.map(({ name }) => {
+      return paths.push({
+        params: {
+          seccion: 'ilustracion',
+          name: name.toLowerCase()
+        }
+      })
+    })
 
-//     const identidad = await fetch(`${API}identidad`)
-//     const identidadData = await identidad.json()
+    const identidad = await fetch(`${API}identidad`)
+    const identidadData = await identidad.json()
 
-//     identidadData.trabajos.map(({ name }) => {
-//       return paths.push({
-//         params: {
-//           seccion: 'identidad',
-//           name: name.toLowerCase()
-//         }
-//       })
-//     })
+    identidadData.trabajos.map(({ name }) => {
+      return paths.push({
+        params: {
+          seccion: 'identidad',
+          name: name.toLowerCase()
+        }
+      })
+    })
 
-//     const rapport = await fetch(`${API}rapport`)
-//     const rapportData = await rapport.json()
+    const rapport = await fetch(`${API}rapport`)
+    const rapportData = await rapport.json()
 
-//     rapportData.trabajos.map(({ name }) => {
-//       return paths.push({
-//         params: {
-//           seccion: 'rapport',
-//           name: name.toLowerCase()
-//         }
-//       })
-//     })
+    rapportData.trabajos.map(({ name }) => {
+      return paths.push({
+        params: {
+          seccion: 'rapport',
+          name: name.toLowerCase()
+        }
+      })
+    })
 
-//     return {
-//       paths,
-//       fallback: false
-//     }
+    return {
+      paths,
+      fallback: false
+    }
 
-//   } catch (error) {
-//     console.log(error)
-//     return { paths: [], fallback: false }
-//   }
-// }
+  } catch (error) {
+    console.log(error)
+    return { paths: [], fallback: false }
+  }
+}
 
 export default Trabajo
