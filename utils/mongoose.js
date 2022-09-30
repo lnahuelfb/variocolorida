64d3b35ea72dafd4a941b96e3e281a75c5b729bf
Nahuel Fernandez Beschtedt <lnahuelfernandezb@gmail.com>
2022-09-30 15:59:59 -0300

    Agregada la base de datos

--- utils/mongoose.js
+++ utils/mongoose.js
@@ -7,7 +7,7 @@ const conn = {
 export const dbConnect = async () => {
   if (conn.isConnected) return
 
-  const db = await connect(process.env.MONGODB_URL)
+  const db = await connect(process.env.MONGODB_URI)
 
   conn.isConnected = db.connections[0].readyState
 
